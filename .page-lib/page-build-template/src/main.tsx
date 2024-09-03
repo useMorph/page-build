651decd57bbc3a54db9d25de6a6c527a10413494
Keita Mitsuhashi <keita.mitsuhashi.83@gmail.com>
2024-09-03 15:55:30 +0900

    Add tailwind

--- .page-lib/page-build-template/src/main.tsx
+++ .page-lib/page-build-template/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { MDXProvider } from "@mdx-js/react";
 import Page from "./Page.mdx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
