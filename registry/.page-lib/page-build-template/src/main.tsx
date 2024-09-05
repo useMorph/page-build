9840b79433a518f42923a19655a015b6f22b6733
Keita Mitsuhashi <keita.mitsuhashi.83@gmail.com>
2024-09-05 13:39:16 +0900

    add @use-morph/page

--- registry/.page-lib/page-build-template/src/main.tsx
+++ registry/.page-lib/page-build-template/src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { MDXProvider } from "@mdx-js/react";
+import { PageProvider } from "@use-morph/page";
 import Page from "./Page.mdx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MDXProvider>
-      <Page />
-    </MDXProvider>
+    <PageProvider>
+      <MDXProvider>
+        <Page />
+      </MDXProvider>
+    </PageProvider>
   </StrictMode>
 );
