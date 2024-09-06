18e5dbfb5a9442a38a635613e35643d7b096b7c8
Keita Mitsuhashi <keita.mitsuhashi.83@gmail.com>
2024-09-06 20:21:17 +0900

    Add css

--- registry/.page-lib/page-build-template/src/main.tsx
+++ registry/.page-lib/page-build-template/src/main.tsx
@@ -4,6 +4,7 @@ import { MDXProvider } from "@mdx-js/react";
 import { PageProvider } from "@use-morph/page";
 import Page from "./Page.mdx";
 import "./index.css";
+import "@use-morph/page/css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
